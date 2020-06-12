ca9776a70154e894c2a6cccad6efca0ff9e370ae
White Ace <whiteace@sina.com>
2020-06-12 17:29:43 +0800

    fix: version

--- App.tsx
+++ App.tsx
@@ -2,16 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import { ThemeProvider } from "react-native-elements";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  ActionSheetProvider,
+  connectActionSheet,
+} from "@expo/react-native-action-sheet";
 import PushNotification from "./components/PushNotification";
 import Main from "./Main";
 import Pets from "./Pets";
 import Store from "./redux/Store";
 import useCachedResources from "./hooks/useCachedResources";
-
-import {
-  ActionSheetProvider,
-  connectActionSheet,
-} from "@expo/react-native-action-sheet";
 
 interface ITheme {
   colors: object;
